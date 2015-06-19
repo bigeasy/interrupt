d43949b9ecb25d8343ced2f45f01a77262ca2746
Alan Gutierrez <alan@prettyrobots.com>
2015-06-19 02:30:05 -0500

    Draft of typed exceptions.

--- interrupt.js
+++ interrupt.js
@@ -1,1 +1,36 @@
-module.exports = 1
+var slice = [].slice
+
+// todo: create strings parser.
+function Interrupt () {
+    this._types = {}
+}
+
+Interrupt.prototype.panic = function (error, type) {
+    this._types[type] || (this._types[type] = {})
+    error.message = error.type = type
+    error.typeIdentifier = this._types[type]
+    var context = error.context = {}
+    slice.call(arguments, 2).forEach(function (values) {
+        for (var key in values) {
+            context[key] = values[key]
+        }
+    })
+    error.context = context
+    throw error
+}
+
+Interrupt.prototype.rescue = function (catcher) {
+    return function (error) {
+        if (error) {
+            if (error.type && this._types[error.type] === error.typeIdentifier) {
+                catcher(error)
+            } else {
+                throw error
+            }
+        }
+    }.bind(this)
+}
+
+module.exports = function (messages) {
+    return new Interrupt
+}
--- t/interrupt/interrupt.t.js
+++ t/interrupt/interrupt.t.js
@@ -1,19 +1,36 @@
-require('proof')(1, prove)
+require('proof')(2, prove)
+
+/*
+    ___ strings: en_US ___
+
+        convert : value, outcome ~ You failed to convert %s to %d.
+        outcome : count, count ~ You have %d #{cat/cats}.
+
+    ___ en_US ___
+ */
 
 function prove (assert) {
-    var interrupt = require('../..')
-    assert(interrupt, 'require')
-    return
+    var interrupt = require('../..')()
     try {
-        interrupt.panic('type', { value: 1 })
+        interrupt.panic(new Error, 'convert', { value: 1 })
     } catch (error) {
         interrupt.rescue(function (error) {
             switch (error.type) {
-            case "type":
-                assert(error.context, { value: 1 }, 'context')
+            case 'convert':
+                assert(error.context, { value: 1 }, 'rescue')
             }
         })(error)
     }
 
+    try {
+        throw new Error('rethrown')
+    } catch (error) {
+        try {
+            interrupt.rescue(function () {})(error)
+        } catch (error) {
+            assert(error.message, 'rethrown', 'rethrow')
+        }
+    }
 
+    interrupt.rescue(function () {})(null, 1)
 }
