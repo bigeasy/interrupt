dc0741e3916c3fe7c205f44a78b5da86c3a8d240
Alan Gutierrez <alan@prettyrobots.com>
2016-06-20 07:15:36 -0500

    Change naming split, add `assert`.

--- bootstrap.js
+++ bootstrap.js
@@ -3,32 +3,27 @@ var util = require('util')
 
 exports.createInterrupterCreator = function (_Error) {
     return function (path) {
-        var ejector = function (name, depth, context, properties) {
+        var ejector = function (options) {
+            var name = options.name
+            var context = options.context || {}
+            var properties = options.properties || {}
+            var callee = options.callee || ejector
             var vargs = slice.call(arguments)
-            name = vargs.shift()
-            typeof vargs[0] == 'number' ? depth = vargs.shift() : 4
-            context = vargs.shift() || {}
-            properties = vargs.shift() || {}
+            var depth = options.depth || null
             var keys = Object.keys(context).length
             var body = ''
             var dump = ''
             var stack = ''
-            if (keys != 0) {
-                var cause = null
+            if (keys != 0 || options.cause) {
                 body = '\n'
-                if (context.cause) {
-                    cause = properties.cause = context.cause
-                    delete context.cause
-                    keys--
-                }
                 if (keys != 0) {
                     dump = '\n' + util.inspect(context, { depth: depth }) + '\n'
                 }
-                if (cause != null) {
-                    dump += '\ncause: ' + cause.stack + '\n\nstack:'
+                if (options.cause != null) {
+                    dump += '\ncause: ' + options.cause.stack + '\n\nstack:'
                 }
             }
-            var message = path + ':' + name + body + dump
+            var message = path + '#' + name + body + dump
             var error = new Error(message)
             for (var key in context) {
                 error[key] = context[key]
@@ -36,10 +31,23 @@ exports.createInterrupterCreator = function (_Error) {
             for (var key in properties) {
                 error[key] = properties[key]
             }
+            if (options.cause) {
+                error.cause = options.cause
+            }
             if (_Error.captureStackTrace) {
-                _Error.captureStackTrace(error, ejector)
+                _Error.captureStackTrace(error, options.callee || ejector)
             }
             return error
+        }
+        var assert = ejector.assert = function (condition, options) {
+            if (!condition) {
+                var copy = {}
+                for (var key in options) {
+                    copy[key] = options[key]
+                }
+                copy.callee = assert
+                throw ejector(options)
+            }
         }
         return ejector
     }
--- t/interrupt.t.js
+++ t/interrupt.t.js
@@ -1,4 +1,4 @@
-require('proof')(7, prove)
+require('proof')(8, prove)
 
 function prove (assert) {
     var interrupt = require('..').createInterrupter('bigeasy.example')
@@ -6,26 +6,29 @@ function prove (assert) {
         try {
             throw new Error('foo')
         } catch (e) {
-            throw interrupt('bar', {
-                url: 'http://127.0.0.1:8080/foo',
-                statusCode: 404,
-                headers: {
-                    sent: {
-                        'content-type': 'text/plain',
-                        'content-length': '10'
+            throw interrupt({
+                name: 'bar',
+                context: {
+                    url: 'http://127.0.0.1:8080/foo',
+                    statusCode: 404,
+                    headers: {
+                        sent: {
+                            'content-type': 'text/plain',
+                            'content-length': '10'
+                        },
+                        received: {
+                            'content-type': 'text/plain',
+                            'content-length': '10'
+                        }
                     },
-                    received: {
-                        'content-type': 'text/plain',
-                        'content-length': '10'
-                    }
                 },
-                cause: e
-            }, {
-                value: 1
+                properties: {
+                    value: 1
+                }
             })
         }
     } catch (e) {
-        assert(/^bigeasy.example:bar$/m.test(e.message), 'message')
+        assert(/^bigeasy.example#bar$/m.test(e.message), 'message')
         assert(e.value, 1, 'properties set')
         assert(e.statusCode, 404, 'context set')
     }
@@ -33,31 +36,39 @@ function prove (assert) {
         try {
             throw new Error('foo')
         } catch (e) {
-            throw interrupt('bar', { cause: e })
+            throw interrupt({ name: 'bar', cause: e })
         }
     } catch (e) {
         console.log(e.stack)
-        assert(/^bigeasy.example:bar$/m.test(e.message), 'no context')
+        assert(/^bigeasy.example#bar$/m.test(e.message), 'no context')
     }
     try {
-        throw interrupt('bar', 2, { key: 'value' })
+        throw interrupt({ name: 'bar', depth: 2, key: 'value' })
     } catch (e) {
         console.log(e.stack)
-        assert(/^bigeasy.example:bar$/m.test(e.message), 'no cause')
+        assert(/^bigeasy.example#bar$/m.test(e.message), 'no cause')
     }
     try {
-        throw interrupt('bar')
+        throw interrupt({ name: 'bar' })
     } catch (e) {
         console.log(e.stack)
-        assert(/^bigeasy.example:bar$/m.test(e.message), 'nothing but message')
+        assert(/^bigeasy.example#bar$/m.test(e.message), 'nothing but message')
+    }
+
+    interrupt.assert(true, { name: 'assert' })
+    try {
+        interrupt.assert(false, { name: 'assert' })
+    } catch (e) {
+        console.log(e.stack)
+        assert(/^bigeasy.example#assert$/m.test(e.message), 'assert failed')
     }
 
     interrupt = require('../bootstrap').createInterrupterCreator({})('bigeasy.example')
 
     try {
-        throw interrupt('bar')
+        throw interrupt({ name: 'bar' })
     } catch (e) {
         console.log(e.stack)
-        assert(/^bigeasy.example:bar$/m.test(e.message), 'no captureStackTrace')
+        assert(/^bigeasy.example#bar$/m.test(e.message), 'no captureStackTrace')
     }
 }
