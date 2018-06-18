404624b78e83fa25d11cb5d126a19ce19389b7ed
Alan Gutierrez <alan@prettyrobots.com>
2018-06-18 02:12:10 -0500

    Complete coverage of parser.

--- t/parse.t.js
+++ t/parse.t.js
@@ -1,9 +1,12 @@
-require('proof')(1, prove)
+require('proof')(3, prove)
 
 function prove (okay) {
     var fs = require('fs')
     var path = require('path')
     var stack = fs.readFileSync(path.join(__dirname, 'parse.txt'), 'utf8')
     var parse = require('../parse')
-    okay(parse(stack).causes[2], '1', 'parsed nested')
+    var parsed = parse(stack)
+    okay(parsed.context, { a: 1 }, 'parsed context')
+    okay(parsed.causes[2], '1', 'parsed nested')
+    okay(parse(''), null, 'parsed nope')
 }
