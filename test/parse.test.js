cb2c0b189945a8a391353c3cb372b777adfe7ae3
Alan Gutierrez <alan@prettyrobots.com>
2019-10-04 16:16:34 -0500

    Revert tests to Proof.
    See #72.

--- test/parse.test.js
+++ test/parse.test.js
@@ -1,14 +1,11 @@
-describe('parse', () => {
+require('proof')(4, (okay) => {
     const fs = require('fs')
-    const assert = require('assert')
     const path = require('path')
-    it('can parse', () => {
-        const stack = fs.readFileSync(path.join(__dirname, 'parse.txt'), 'utf8')
-        const parse = require('../parse')
-        const parsed = parse(stack)
-        assert.deepStrictEqual(parsed.context, { a: 1 }, 'parsed context')
-        assert.equal(parsed.causes[2], '1', 'parsed nested')
-        assert.equal(parse(''), null, 'parsed nope')
-        assert.deepStrictEqual(parsed.causes[1].contexts[0], { a: { c: 3  } }, 'parsed error context')
-    })
+    const stack = fs.readFileSync(path.join(__dirname, 'parse.txt'), 'utf8')
+    const parse = require('../parse')
+    const parsed = parse(stack)
+    okay(parsed.context, { a: 1 }, 'parsed context')
+    okay(parsed.causes[2], '1', 'parsed nested')
+    okay(parse(''), null, 'parsed nope')
+    okay(parsed.causes[1].contexts[0], { a: { c: 3  } }, 'parsed error context')
 })
