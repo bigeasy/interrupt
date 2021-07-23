03eda07f714673d386148b61a388ff433fc07509
Alan Gutierrez <alan@prettyrobots.com>
2021-07-23 00:12:11 -0500

    Try to fix broken Actions build.:wq

--- test/old.readme.t.js
+++ test/old.readme.t.js
@@ -105,7 +105,8 @@ require('proof')(206, async okay => {
 
         const tmp = path.join(__dirname, 'tmp')
 
-        await fs.rmdir(tmp, { recursive: true })
+        const { coalesce } = require('extant')
+        await coalesce(fs.rm, fs.rmdir).call(fs, tmp, { recursive: true })
         await fs.mkdir(path.join(tmp, 'eisdir', 'config.json'), { recursive: true })
         await fs.mkdir(path.join(tmp, 'enoent'))
         await fs.mkdir(path.join(tmp, 'good'))
